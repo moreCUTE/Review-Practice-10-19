98a8431eb4dd25185aa61f6a5951923a6fe1baa0
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2022-10-19 13:25:09 -0600

    Create review.py

---
+++ review.py
@@ -0,0 +1,108 @@
+print("give me a color")
+color = input()
+if color == "blue":
+    print("the sky is blue")
+elif color == "green":
+    print("the grass is green")
+else:
+    print("I dont knwo that color sorry pimp")
+    
+print("-------------------")
+print("how old are you pimp")
+
+age = input()
+if age < "43":
+    print("u too young grow up")
+elif age > "43":
+    print("u old pimp")
+elif age == "43":
+    print("u perfect to play thjis game enjoy pimp")
+
+print("-------------------")
+
+for i in range(50, 30, -3):
+    print(i) 
+
+print("-------------------")
+
+for j in range(10, 60, 5):
+    print(j)
+    
+print("-------------------")
+
+#print("say sum")
+#x = input()
+
+#while x != "stop":
+  #  print (x)
+
+print("-------------------")
+
+def integers(q, f):
+    
+    print(q % f)
+
+q = 9
+f = 3
+integers(q, f)
+
+
+print("give me a color")
+color = input()
+if color == "blue":
+    print("the sky is blue")
+elif color == "green":
+    print("the grass is green")
+else:
+    print("I dont knwo that color sorry pimp")
+    
+print("-------------------")
+print("how old are you pimp")
+
+age = input()
+if age < "43":
+    print("u too young grow up")
+elif age > "43":
+    print("u old pimp")
+elif age == "43":
+    print("u perfect to play thjis game enjoy pimp")
+
+print("-------------------")
+
+for i in range(50, 30, -3):
+    print(i) 
+
+print("-------------------")
+
+for j in range(10, 60, 5):
+    print(j)
+    
+print("-------------------")
+
+#print("say sum")
+#x = input()
+
+#while x != "stop":
+  #  print (x)
+  
+#it brokey i forgor
+
+print("-------------------")
+
+def integers(q, f):
+    
+    print(q % f)
+
+q = 9
+f = 3
+integers(q, f)
+
+print("-------------------")
+
+def snorfle():
+    for r in range(0, 14, 1):
+        print("snorfle")
+        
+snorfle()
+
+
